3c79724abfc9b1d3b0f7270e7684707c414047fd
jibril <jazimjibril@gmail.com>
2024-02-29 02:00:10 +0300

    Add curved background div and new logo image 🦞

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,7 +1,14 @@
 <template>
-  <div></div>
+  <div class="">
+    <div class="bg-primary h-[90vh] curved-div"></div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.curved-div {
+  border-bottom-left-radius: 50% 30px;
+  border-bottom-right-radius: 50% 30px;
+}
+</style>
