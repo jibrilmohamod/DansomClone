276b3bb0b65658621cafe23333023f63709468c4
jibril <jazimjibril@gmail.com>
2024-02-28 20:38:27 +0300

    Added PrimeVue 👍

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+  css: ["~/assets/css/tailwind.css"],
+  primevue: {
+    unstyled: true,
+  },
+});
