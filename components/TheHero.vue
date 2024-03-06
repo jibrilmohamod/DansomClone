652ab27af09024515bf4f457cf1d5374ca94a8fd
Jibril <jazimjibril@gmail.com>
2024-03-06 21:53:04 +0300

    Update styles and fonts in TheNav.vue, nuxt.config.ts, and tailwind.config.ts

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="pb-10 ">
     <div
-         class="bg-[url(https://preview.redd.it/8rrqh5rpwj131.jpg?width=640&crop=smart&auto=webp&s=50b2f52ecb71901da7b38f685f304c43bb996d43)] h-[90vh] curved-div bg-blend-overlay bg-slate-500 bg-cover">
+         class="bg-[url(https://preview.redd.it/8rrqh5rpwj131.jpg?width=640&crop=smart&auto=webp&s=50b2f52ecb71901da7b38f685f304c43bb996d43)] h-[90vh] curved-div bg-blend-overlay bg-black bg-opacity-50 bg-cover">
 
       <TheNav />
+
+      <!-- call to action  -->
+      <div class="h-[76vh] outline outline-red-500 px-6 flex items-center">
+
+        <!-- Big Header Text -->
+        <div class="flex flex-col items-center justify-center h-[50vh]">
+          <h1 class="text-6xl font-[Playfair Display] text-center ">
+            One Word: <span class=" text-white block">
+              Patner
+            </span>
+          </h1>
+          <p class="text-2xl text-white">Join the community and share your stories with the world</p>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
--- components/TheNav.vue
+++ components/TheNav.vue
@@ -13,16 +13,16 @@
 </script>
 
 <template>
-  <div class="w-full px-4 py-5 bg-transparent">
+  <div class="w-full px-4 py-5 bg-white">
     <NavigationMenu class="w-96">
       <NavigationMenuList>
         <!-- menu item -->
         <ul class="flex w-[90vw] items-center justify-between">
-          <li class="basis-0 ">
+          <li class="outline">
             <NavigationMenuItem>
               <NavigationMenuLink href="/">
-                <Icon name="bytesize:menu"
-                      class="text-[40px] text-white" />
+                <Icon name="quill:hamburger"
+                      class="text-[40px] text-quaternary" />
 
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -34,7 +34,7 @@
               <NavigationMenuLink href="/">
                 <img src="/dansom-logo.png"
                      alt="logo"
-                     class="h-12" />
+                     class="h-10" />
               </NavigationMenuLink>
             </NavigationMenuItem>
 
