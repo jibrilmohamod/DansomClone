dda95d387fba199ebfe1e72dee16de0bfc189760
Jibril <jazimjibril@gmail.com>
2024-04-16 20:48:15 +0300

    Refactor font styles in TheCarousel.vue and TheHero.vue components

--- components/TheCarousel.vue
+++ components/TheCarousel.vue
@@ -1,63 +1,66 @@
 <template>
-    <div id="app"
-         class="h-screen">
-        <div class="relative slide overflow-hidden">
-            <div class="carousel-inner relative overflow-hidden w-full">
-                <div v-for="(img, i) in images"
-                     :id="`slide-${i}`"
-                     :key="i"
-                     :class="`${active === i ? 'active' : 'left-full'}`"
-                     class="carousel-item inset-0 relative w-full transform transition-all duration-1000 ease-in-out">
-                    <NuxtImg class="block w-full h-screen object-cover"
-                             :src="img"
-                             provider="cloudinary"
-                             format="webp"
-                             sizes="sm:800px md:50vw xl:1200px"
-                             :modifiers="{ effect: 'colorize:40', color: 'black' }"
-                             alt="First slide" />
-                </div>
-            </div>x
-        </div>
+ <div id="app" class="h-screen">
+  <div class="relative slide overflow-hidden">
+   <div class="carousel-inner relative overflow-hidden w-full">
+    <div
+     v-for="(img, i) in images"
+     :id="`slide-${i}`"
+     :key="i"
+     :class="`${active === i ? 'active' : 'left-full'}`"
+     class="carousel-item inset-0 relative w-full transform transition-all duration-1000 ease-in-out"
+    >
+     <NuxtImg
+      class="block w-full h-screen object-cover"
+      :src="img"
+      provider="cloudinary"
+      format="webp"
+      sizes="sm:800px md:50vw xl:1200px"
+      :modifiers="{ effect: 'colorize:40', color: 'black' }"
+      alt="First slide"
+     />
     </div>
+   </div>
+  </div>
+ </div>
 </template>
 
 <script setup>
-const images = ref([
-    "v1713240642/front-view-kids-posing-together-with-ball_23-2149479910_xylyvc.jpg",
-    "v1713240714/smiling-young-african-girl-wearing-traditional-clothing-classroom-with-students_996993-23087_ygimi3.jpg",
-    "v1713240777/countryside-women-out-field-posing_23-2148761775_wo8svm.jpg",
-    "v1713240810/close-up-farmer-holding-plants_23-2149142896_fuhkxy.jpg",
-])
+ const images = ref([
+  "v1713240642/front-view-kids-posing-together-with-ball_23-2149479910_xylyvc.jpg",
+  "v1713240714/smiling-young-african-girl-wearing-traditional-clothing-classroom-with-students_996993-23087_ygimi3.jpg",
+  "v1713240777/countryside-women-out-field-posing_23-2148761775_wo8svm.jpg",
+  "v1713240810/close-up-farmer-holding-plants_23-2149142896_fuhkxy.jpg",
+ ])
 
-let active = ref(0)
+ let active = ref(0)
 
-onMounted(() => {
-    let i = 0
-    setInterval(() => {
-        if (i > images.value.length - 1) {
-            i = 0
-        }
-        active.value = i
-        i++
-    }, 5000)
-})
+ onMounted(() => {
+  let i = 0
+  setInterval(() => {
+   if (i > images.value.length - 1) {
+    i = 0
+   }
+   active.value = i
+   i++
+  }, 5000)
+ })
 </script>
 
 <style scoped>
-.left-full {
-    left: -100%;
-}
+ .left-full {
+  left: -100%;
+ }
 
-.carousel-item {
-    float: left;
-    position: relative;
-    display: block;
-    width: 100%;
-    margin-right: -100%;
-    backface-visibility: hidden;
-}
+ .carousel-item {
+  float: left;
+  position: relative;
+  display: block;
+  width: 100%;
+  margin-right: -100%;
+  backface-visibility: hidden;
+ }
 
-.carousel-item.active {
-    left: 0;
-}
+ .carousel-item.active {
+  left: 0;
+ }
 </style>
--- components/TheHero.vue
+++ components/TheHero.vue
@@ -3,9 +3,44 @@
   <TheCarousel class="" />
   <div class="curved-div b bg-cover bg-center absolute inset-0">
    <TheNav />
-   <div class="h-[85vh] w-full outline md:flex items-center hidden">
-    <div class="w-1/2 h-full"></div>
-    <div class="w-[40%] h-5/6 bg-primary rounded-full bg-opacity-60"></div>
+   <div class="h-[85vh] w-full flex items-center justify-center">
+    <div class="w-1/2 h-full hidden md:block"></div>
+    <div
+     class="md:w-[40%] md:h-5/6 bg-primary rounded-full bg-opacity-60 flex items-center h-80 w-80 flex-col justify-center gap-2 p-4 md:gap-6"
+     data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom"
+     data-aos-duration="1000"
+    >
+     <h1
+      class="text-white text-3xl text-center font font-Pridi md:text-5xl"
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1200"
+     >
+      Powerful transformations across the Horn of Africa
+     </h1>
+     <p
+      class="text-center text-3xl font-Zilla"
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1400"
+     >
+      Dansom is a leading consultancy firm in the Horn of Africa. We provide a
+      range of services to our clients
+     </p>
+     <button
+      class="bg-white text-black font-Merriweather py-2 px-4 rounded-2xl flex justify-center gap-x-2 md:text-2xl md:py-5 md:px-6 md:rounded-3xl md:gap-x-4"
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1400"
+     >
+      <Icon
+       name="lets-icons:sun-light"
+       class="text-black text-xl md:text-3xl"
+      />
+      <p>Learn More</p>
+     </button>
+    </div>
    </div>
   </div>
  </div>
