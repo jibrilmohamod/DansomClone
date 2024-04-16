93f967014c583592cbf9a9bd61d932f0799d866d
jibril <jazimjibril@gmail.com>
2024-04-16 19:04:05 +0300

    Refactor OurExpertise and UiComp components for improved styling and responsiveness

--- components/UiComp.vue
+++ components/UiComp.vue
@@ -1,59 +1,72 @@
 <template>
-     <div class="py-4 flex justify-center">
-                <h4 class=" leading-tight 2xl:text-6xl font-DM text-5xl text-center w-1/2">
-                    {{ header }}
-                </h4>
-            </div>
-    <div class="pt-8 pl-6 pr-3 pb-4 md:container md:m-auto md:flex md:gap-x-6 2xl:px-0 md:px-6">
-       
-        <div class="md:basis-1/2 md:flex md:flex-col h-full">
-            <!-- small text with line to its right -->
-
-
-            <!-- section header -->
-           
-
-            <!-- section body -->
-            <div class="">
-                <!-- paragraph -->
-                <p class="font-Zilla text-[18px] leading-relaxed tracking-wide text-black 2xl:w-3/4 2xl:pb-7">
-                    {{ paragraph }}
-                </p>
-
-                <!-- button and image slots -->
-                <div>
-                    <!-- Button slot -->
-                    <slot name="button"> </slot>
-
-                    <!-- Image slot -->
-                    <slot name="image"> </slot>
-                </div>
-            </div>
+  <div
+    class="md:pt-0 pl-6 pr-3 pb-4 md:container md:m-auto md:flex md:gap-x-6 2xl:px-0 md:px-6"
+    data-aos="fade-up"
+    data-aos-duration="1000"
+  >
+    <div class="md:basis-1/2 md:flex md:flex-col h-full">
+      <div class="md:basis-1/2 md:flex md:flex-col h-full">
+        <!-- small text with line to its right -->
+        <div class="flex items-center md:hidden">
+          <div class="w-1/2 h-0.5 bg-primary"></div>
+          <p
+            class="font-Zilla text-[18px] leading-relaxed tracking-wide text-black 2xl:w-3/4 2xl:pb-7 text-right w-1/2"
+          >
+            {{ smallText }}
+          </p>
         </div>
 
-        <!-- image slot -->
-        <div class="hidden md:flex basis-1/2 m-auto">
-            <slot name="image2"> </slot>
+        <!-- section header -->
+        <div class="py-4">
+          <h4 class="leading-tight md:text-5xl 2xl:text-6xl font-DM text-5xl">
+            {{ header }}
+          </h4>
         </div>
+      </div>
+
+      <!-- section body -->
+      <div class="">
+        <!-- paragraph -->
+        <p
+          class="font-Zilla text-[18px] leading-relaxed tracking-wide text-black 2xl:w-3/4 2xl:pb-7"
+        >
+          {{ paragraph }}
+        </p>
+
+        <!-- button and image slots -->
+        <div>
+          <!-- Button slot -->
+          <slot name="button"> </slot>
+
+          <!-- Image slot -->
+          <slot name="image"> </slot>
+        </div>
+      </div>
     </div>
+
+    <!-- image slot -->
+    <div class="hidden md:flex basis-1/2 m-auto">
+      <slot name="image2"> </slot>
+    </div>
+  </div>
 </template>
 
 <script setup>
 // define props with default values and types
 defineProps({
-    smallText: {
-        type: String,
-        default: "",
-    },
-    header: {
-        type: String,
-        default: "",
-    },
-    paragraph: {
-        type: String,
-        default: " .",
-    },
-})
+  smallText: {
+    type: String,
+    default: "",
+  },
+  header: {
+    type: String,
+    default: "",
+  },
+  paragraph: {
+    type: String,
+    default: " .",
+  },
+});
 </script>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,194 +1,204 @@
 <template>
   <div class="">
     <TheHero />
-    <OurExpertise />
-    <!-- About Us Card  -->
-    <UiComp header="Who We are"
-            small-text="Dansom"
-            paragraph="We are a research and consultancy firm that was established in 2009 to work with humanitarian and development partners and ensure powerful transformations in the development of the horn of Africa. Dansom has since developed a network of thematic and regional experts based in Somalia and Kenya, with a unique level of access to hard-to-reach areas and a strong focus on security and socio-political and economic development in Eastern Africa.">
 
+    <!-- About Us Card  -->
+    <UiComp
+      header="Who We are"
+      small-text="Dansom"
+      paragraph="We are a research and consultancy firm that was established in 2009 to work with humanitarian and development partners and ensure powerful transformations in the development of the horn of Africa. Dansom has since developed a network of thematic and regional experts based in Somalia and Kenya, with a unique level of access to hard-to-reach areas and a strong focus on security and socio-political and economic development in Eastern Africa."
+    >
       <template #image>
         <ImageCard class="pt-10" />
       </template>
       <template #button>
         <div class="flex pt-8 pb-0">
-          <Button navigateTo="/"
-                  class="bg-primary text-white font-Montserrat px-6 h-12 text-lg">
+          <Button
+            navigateTo="/"
+            class="bg-primary text-white font-Montserrat px-6 h-12 text-lg"
+          >
             Learn More
           </Button>
         </div>
       </template>
       <template #image2>
-                <div class="w-full px-2 flex gap-2"
-                     data-aos="fade-up"
-                     data-aos-duration="1000">
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:10', color: 'black' }"
-                             sizes="sm:100px md:50vw lg:600px"
-                             provider="cloudinary"
-                             src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
-                             class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black" />
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:20', color: 'black' }"
-                             sizes="sm:100vw md:50vw lg:800px"
-                             provider="cloudinary"
-                             src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
-                             class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black" />
-                </div>
-            </template>
-
+        <div
+          class="w-full px-2 flex gap-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:10', color: 'black' }"
+            sizes="sm:100px md:50vw lg:600px"
+            provider="cloudinary"
+            src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
+            class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black"
+          />
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:20', color: 'black' }"
+            sizes="sm:100vw md:50vw lg:800px"
+            provider="cloudinary"
+            src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
+            class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black"
+          />
+        </div>
+      </template>
     </UiComp>
     <!-- services card -->
+    <OurExpertise />
 
-    <UiComp header="Our Expertise and Solutions"
-            paragraph="With access to various locations within Somalia, DANSOM provides high quality tailor made services to the clients’ needs, goals and objectives. We provide an array of services including but not limited to third party monitoring and evaluation, political economy analysis ,security advisory and formative research and policy advisory services">
-      <template #image>
-        <ServiceCard v-for="service in services"
-                     :key="service.title"
-                     :icon="service.icon"
-                     :title="service.title"
-                     :subtitle="service.subtitle" />
-      </template>
-      <template #image2>
-                <div class="w-full px-2 flex gap-2"
-                     data-aos="fade-up"
-                     data-aos-duration="1000">
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:10', color: 'black' }"
-                             sizes="sm:100px md:50vw lg:600px"
-                             provider="cloudinary"
-                             src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
-                             class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black" />
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:20', color: 'black' }"
-                             sizes="sm:100vw md:50vw lg:800px"
-                             provider="cloudinary"
-                             src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
-                             class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black" />
-                </div>
-            </template>
-    </UiComp>
     <!-- Patners and Portfolio card -->
 
-    <UiComp header="Our Partners and Portfolio"
-            paragraph="We have worked with a number of partners and clients in the horn of Africa. Our portfolio includes work with the United Nations, the European Union, the World Bank, the Swiss Agency for Development, the Government of Somalia, and a number of international and local NGOs.">
+    <UiComp
+      header="Our Partners and Portfolio"
+      paragraph="We have worked with a number of partners and clients in the horn of Africa. Our portfolio includes work with the United Nations, the European Union, the World Bank, the Swiss Agency for Development, the Government of Somalia, and a number of international and local NGOs."
+    >
       <template #image>
         <LogoCarousel />
         <!-- learn more button  -->
         <div class="flex pt-8 pb-0">
-          <Button navigateTo="/"
-                  class="bg-primary text-white font-Montserrat px-6 h-12 text-lg">
+          <Button
+            navigateTo="/"
+            class="bg-primary text-white font-Montserrat px-6 h-12 text-lg"
+          >
             Our Projects
           </Button>
         </div>
       </template>
 
       <template #image2>
-        <div class="w-full px-2 flex gap-2  items-center">
-
-          <Image3Card
-                      image="https://img.freepik.com/free-photo/view-woman-working-agricultural-sector-celebrate-labour-day-women_23-2151252044.jpg?t=st=1713241743~exp=1713245343~hmac=07c24fed27d76d5827409399df2f0a1c0475920088583e76dc98245115b8f436&w=740" />
-          <Image2Card />
+        <div
+          class="w-full px-2 flex gap-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:10', color: 'black' }"
+            sizes="sm:100px md:50vw lg:600px"
+            provider="cloudinary"
+            src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
+            class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black"
+          />
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:20', color: 'black' }"
+            sizes="sm:100vw md:50vw lg:800px"
+            provider="cloudinary"
+            src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
+            class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black"
+          />
         </div>
-
       </template>
-
     </UiComp>
-
-
 
     <!-- Contact Us Card  -->
 
-    <UiComp header="We can help you navigate what's next"
-            paragraph="With more than 10 years of experience in the horn of Africa, we have the expertise and the network to help you navigate the complex socio-political and economic landscape of the region. CONTACT US today to learn more about our services and how we can help you achieve your goals.">
+    <UiComp
+      header="We can help you navigate what's next"
+      paragraph="With more than 10 years of experience in the horn of Africa, we have the expertise and the network to help you navigate the complex socio-political and economic landscape of the region. CONTACT US today to learn more about our services and how we can help you achieve your goals."
+    >
       <template #image>
-        <Button navigateTo="/"
-                class="bg-primary text-white font-Montserrat px-6 h-12 text-lg">
+        <Button
+          navigateTo="/"
+          class="bg-primary text-white font-Montserrat px-6 h-12 text-lg"
+        >
           Get in Touch
         </Button>
-        <ImageCard image="https://res.cloudinary.com/dckmlqzgd/image/upload/v1713240777/countryside-women-out-field-posing_23-2148761775_wo8svm.jpg"
-                   class="pt-5" />
+        <ImageCard
+          image="https://res.cloudinary.com/dckmlqzgd/image/upload/v1713240777/countryside-women-out-field-posing_23-2148761775_wo8svm.jpg"
+          class="pt-5"
+        />
       </template>
       <template #image2>
-                <div class="w-full px-2 flex gap-2"
-                     data-aos="fade-up"
-                     data-aos-duration="1000">
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:10', color: 'black' }"
-                             sizes="sm:100px md:50vw lg:600px"
-                             provider="cloudinary"
-                             src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
-                             class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black" />
-                    <NuxtImg loading="lazy"
-                             format="webp"
-                             alt="image"
-                             :modifiers="{ effect: 'colorize:20', color: 'black' }"
-                             sizes="sm:100vw md:50vw lg:800px"
-                             provider="cloudinary"
-                             src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
-                             class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black" />
-                </div>
-            </template>
+        <div
+          class="w-full px-2 flex gap-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:10', color: 'black' }"
+            sizes="sm:100px md:50vw lg:600px"
+            provider="cloudinary"
+            src="v1713240887/countryside-worker-pushing-wheelbarrow-with-peanuts_23-2148761782_qigkes.jpg"
+            class="w-1/2 h-[60vh] object-cover md:object-cover shadow-2xl shadow-black"
+          />
+          <NuxtImg
+            loading="lazy"
+            format="webp"
+            alt="image"
+            :modifiers="{ effect: 'colorize:20', color: 'black' }"
+            sizes="sm:100vw md:50vw lg:800px"
+            provider="cloudinary"
+            src="v1713240956/medium-shot-people-selling-bananas_23-2150830598_epjgud.jpg"
+            class="w-1/2 h-[40vh] m-auto object-cover shadow-xl shadow-black"
+          />
+        </div>
+      </template>
     </UiComp>
-
-
-
   </div>
 </template>
 
-<script setup
-        lang="ts">
-        import { Button } from '@/components/ui/button'
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
 
-        let services = [
-          {
-            icon: "arcticons:adobe-analytics",
-            title: "Political Economy Analysis",
-            subtitle: "Political Economy Analysis (PEA) in Somalia to inform programming and policy development."
-          },
-          {
-            icon: "carbon:data-analytics",
-            title: "Third Party Monitoring and Evaluation",
-            subtitle: "Third Party Monitoring and Evaluation (TPME) of humanitarian and development projects in Somalia."
-          },
-          {
-            icon: "material-symbols-light:chart-data-outline-rounded",
-            title: "Formative Research and Policy Advisory Services",
-            subtitle: "Formative Research and Policy Advisory Services to inform programming and policy development."
-          },
-          {
-            icon: "ph:leaf-thin",
-            title: "Environmental Impact Assessment (EIA) & Monitoring",
-            subtitle: "Environmental Impact Assessment (EIA) & Monitoring of humanitarian and development projects in Somalia."
-          },
-          {
-            icon: "pepicons-pencil:people",
-            title: "Organizational Capacity Building & HR",
-            subtitle: "Organizational Capacity Building & Human Resource Management for local and international organizations in Somalia."
-          },
-          {
-            icon: "ph:gear-six-thin",
-            title: "Operational and Logistics Management",
-            subtitle: "Operational and Logistics Management for local and international organizations in Somalia."
-          },
-          {
-            icon: "la:people-carry",
-            title: "Community Engagement Strategies",
-            subtitle: "Community Engagement Strategies for local and international organizations in Somalia."
-          },
-
-        ]
+let services = [
+  {
+    icon: "arcticons:adobe-analytics",
+    title: "Political Economy Analysis",
+    subtitle:
+      "Political Economy Analysis (PEA) in Somalia to inform programming and policy development.",
+  },
+  {
+    icon: "carbon:data-analytics",
+    title: "Third Party Monitoring and Evaluation",
+    subtitle:
+      "Third Party Monitoring and Evaluation (TPME) of humanitarian and development projects in Somalia.",
+  },
+  {
+    icon: "material-symbols-light:chart-data-outline-rounded",
+    title: "Formative Research and Policy Advisory Services",
+    subtitle:
+      "Formative Research and Policy Advisory Services to inform programming and policy development.",
+  },
+  {
+    icon: "ph:leaf-thin",
+    title: "Environmental Impact Assessment (EIA) & Monitoring",
+    subtitle:
+      "Environmental Impact Assessment (EIA) & Monitoring of humanitarian and development projects in Somalia.",
+  },
+  {
+    icon: "pepicons-pencil:people",
+    title: "Organizational Capacity Building & HR",
+    subtitle:
+      "Organizational Capacity Building & Human Resource Management for local and international organizations in Somalia.",
+  },
+  {
+    icon: "ph:gear-six-thin",
+    title: "Operational and Logistics Management",
+    subtitle:
+      "Operational and Logistics Management for local and international organizations in Somalia.",
+  },
+  {
+    icon: "la:people-carry",
+    title: "Community Engagement Strategies",
+    subtitle:
+      "Community Engagement Strategies for local and international organizations in Somalia.",
+  },
+];
 </script>
 
-<style lang="scss"
-       scoped></style>
+<style lang="scss" scoped></style>
