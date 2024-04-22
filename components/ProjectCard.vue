8d1492dd4b7dd3c6fb3fdae570a0892815016c54
jibril <jazimjibril@gmail.com>
2024-04-22 16:24:03 +0300

    Refactor ProjectCard.vue component styles and animation

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,23 +1,27 @@
 <template>
- <div data-aos="fade-up" data-aos-duration="800">
+ <div data-aos="fade-left" data-aos-duration="800">
   <NuxtLink :to="slug">
    <div
-    class="bg-white shadow-2xl rounded-lg overflow-hidden h-60 hover:scale-110 hover:duration-200 hover:ease-linear"
+    class="bg-white shadow-2xl rounded-lg overflow-hidden h-60 hover:scale-105 hover:duration-200 hover:ease-linear"
    >
     <div class="h-full px-3 pb-2">
      <!-- logo  -->
-     <div class="flex justify-center items-center h-1/2">
+     <div
+      class="flex justify-center items-center h-1/2 border-b border-1 border-slate-300"
+     >
       <NuxtImg
        :src="icon"
        alt="logo"
-       class="object-contain h-24 w-fit"
-       format="webp"
+       class="object-contain h-24 w-full"
        loading="lazy"
+       quality="100"
        v-if="icon"
       />
      </div>
-     <div class="h-1/2 flex items-center">
-      <h3 class="font-Lora text-lg text-pretty text-quaternary">
+     <div class="h-1/2 flex items-center justify-center">
+      <h3
+       class="font-Merriweather text-lg text-pretty text-quaternary text-center"
+      >
        {{ title }}
       </h3>
      </div>
--- pages/Portfolio/index.vue
+++ pages/Portfolio/index.vue
@@ -24,11 +24,39 @@
   <!-- projects -->
   <div class="bg-tertiary container m-auto pb-5">
    <OurExpertise title="Our Projects" />
-   <div
-    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:px-5 md:-mt-4"
-   >
+   <div class="">
+    <!-- search and filter bar -->
+    <div class="flex justify-between items-center gap-5 py-5 z-50 px-5">
+     <input
+      type="text"
+      v-model="searchTerm"
+      placeholder="Search Projects"
+      class="border-2 border-slate-300 rounded-md px-3 py-2 w-1/2"
+     />
+     <div class="flex items-center gap-5">
+      <p class="text-white">Filter by:</p>
+      <select
+       v-model="classification"
+       class="border-2 border-slate-300 rounded-md px-3 py-2"
+      >
+       <option value="All">All</option>
+       <option value="Third Party Monitoring & Evaluation">
+        Third Party Monitoring & Evaluation
+       </option>
+
+       <option value="Political Economy Analysis & Security Advisory">
+        Political Economy Analysis & Security Advisory
+       </option>
+       <option value="Formative Research & Policy Analysis">
+        Formative Research & Policy Analysis
+       </option>
+      </select>
+     </div>
+    </div>
+   </div>
+   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:px-5">
     <ProjectCard
-     v-for="project in displayedProjects"
+     v-for="project in filteredProjects"
      :key="project.title"
      :title="project.title"
      :icon="project.icon"
@@ -50,17 +78,6 @@
  import Button from "~/components/ui/button/Button.vue"
  let projects = ref([
   {
-   title: "Swiss Agency for Development and Cooperation (SDC)",
-   classification: "Classification",
-   description:
-    "The current objective is to improve the performance of the Swiss HoA programme. This overall work to improve the programme is called the “Framework for Risk Gover- nance and Adaptive Programming” (FRAP). It will be implemented over a twelve year timeframe. FRAP will initially focus on Soma- lia (including Somaliland).",
-   Location: "Somalia",
-   timeframes: "June 2021 - December 2024",
-   icon: "/download (1).png",
-
-   Ngo: "SDC",
-  },
-  {
    title:
     "The Provision of Monitoring and Evaluation Services in support of UNSOS(Somalia)",
    classification: "Third Party Monitoring & Evaluation",
@@ -70,6 +87,17 @@
    timeframes: "December 2020 - present",
    icon: "Capture1.PNG-removebg-preview.png",
    Ngo: "UNSOS",
+  },
+  {
+   title: "Swiss Agency for Development and Cooperation (SDC)",
+   classification: "Classification",
+   description:
+    "The current objective is to improve the performance of the Swiss HoA programme. This overall work to improve the programme is called the “Framework for Risk Gover- nance and Adaptive Programming” (FRAP). It will be implemented over a twelve year timeframe. FRAP will initially focus on Soma- lia (including Somaliland).",
+   Location: "Somalia",
+   timeframes: "June 2021 - December 2024",
+   icon: "/download (1).png",
+
+   Ngo: "SDC",
   },
   {
    title: "EU Delegation to Somalia (EUD-Somalia)",
@@ -87,7 +115,7 @@
   },
   {
    title: "Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ)",
-   classification: " Political Economy Analysis & Security Advisory ",
+   classification: "Political Economy Analysis & Security Advisory",
    description:
     " Consortium of CMC & Dansom consultancy conducted a peace and conflict assessment (PCA) in Kismayo, Somalia for the Somali Re integration Programme (GIZ SRP) and Fish for Nutrition project (GIZ FNP)",
    Location: "Somalia",
@@ -103,38 +131,131 @@
    Location: "Somalia",
    timeframes: "September 2019 - March 2020",
    icon: "SomaliaWorldBank.jpg",
+   Ngo: "World Bank",
   },
   {
    title: "Technical Assistance on Reorganization & Modernization of MDA’s",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "provide technical assistance to support a diagnostic assessment of the organizational set up of MDAs focusing on their mandates, functions, and organizational structures, staffing requirements and administrative policies, processes and systems",
+   Location: "Somalia",
+   timeframes: "December 2020 - September 2021",
+   icon: "Civil.png",
   },
   {
    title: "United Nations Office for Project Services (UNOPS)",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "Provide regular and independent analysis and assessments of the JPP in Somalia through quantitative and qualitative methodologies to undertake routine monitoring, baseline survey, annual perception survey, socio-political analysis, and measure efficiency.",
+   Location: "Somalia",
+   timeframes: "December 2019 - December 2023",
+   icon: "unops.jpg",
   },
   {
    title: "World Food Programme (WFP)/FAO",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "Research and evaluation services for supporting resilient small holder farming systems in Somalia.We are expected to provide an independent perspective and expand into areas sometimes inaccessible to WFP/FAO staff due to security reasons.",
+   Location: "Somalia",
+   timeframes: "November 2022 - May 2024",
+   icon: "wfpfao.jpg",
   },
   {
    title:
     "The Ministry of Planning, Investment and Economic Development for the Federal Republic of Somalia",
+   classification:
+    "Organizational Capacity Building & Human Resource Management",
+   description:
+    "Consortium of Dansom and PARTICIP are implementing the Somalia National Statistical Capacity Building Project. The project seeks to build the capacity of the National Directorate for Statistics of the Federal Ministry of International Cooperation (FMoPIC) and Statistics Departments of Puntland Ministry of Planning and International Cooperation (PLMoPIC) and Southwest Administration Ministry of Planning and International Cooperation (SWAMoPIC) to enable these institutions to effectively conduct, supervise and coordinate the statistical activities in the areas under their jurisdiction.",
+   Location: "Somalia",
+   timeframes: "February 2018 - August 2019",
+   icon: "ministry-of-planning-400x162.png",
+  },
+  {
+   title: "The Food and Agriculture Organization of the United Nations (FAO)",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "Third party Technical Review to assess implementation and impact of the Somalia Emergency Drought Response and Recovery Project(SEDRP) in all regions in Somalia; Somaliland, Puntland, Jubaland, South-West State, HirShabelle and Galmudug.",
+   Location: "Somalia",
+   timeframes: "December 2017 - December 2018",
+   icon: "FAO_logo.svg",
+  },
+  {
+   title: "The World Bank Group",
+   classification: "Operational & Logistics Management",
+   description:
+    "Dansom partnered with the World Bank Group to facilitate the event management of the Microeconomic and Fiscal Management Program Implemented by the World Bank Group.",
+   Location: "Somalia",
+   timeframes: "September 2016 - December 2016",
+   classification2: "Operational & Logistics Management",
+   description2:
+    "World Bank – Somalia High Frequency Survey. Dansom facilitated cooperation between the government of Somalia and The SHFS team (Altai and World Bank).",
+   icon: "The_World_Bank_logo.svg",
   },
   {
    title: "Somalia Invest Climate Reform Program, IFC-World Bank",
+   classification: "Operational & Logistics Management",
+   description:
+    "DANSOM is providing various services to IFC as part of this project, including the provision of qualified consultants, arranging consultant travel, organizing logistics, managing payments of fees and expenses related to the contract, and obtaining prior approval from the IFC project manager for any relevant subcontracting arrangements.",
+   Location: "Somalia",
+   timeframes: "March 2017 - December 2018",
+   classification2: "Operational & Logistics Management",
+   description2:
+    "Dansom has partnered with IFC, World Bank Group to facilitate and support Public Private Dialogue in Somalia’s economic recovery sector. The recent support being to the Telecommunication sector and providing strategic advice in moving the National Communication Act towards submission and approval by the National Federal Parliament. Dansom also gives advice to IFC-World Bank on Somalia context, political economic analysis and Business development.",
+   icon: "International_Finance_Corporation_logo.svg",
   },
   {
    title: "Horumarinta Elmiga II-Save the Children",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "Final Evaluation of Horumarinta Elmiga II(Education for Empowerment through Cohesive and Harmonized System) was funded by the European Union and  implemented by Save the Children  as the lead agency ,Care International and Norwegian Refugees Council (NRC), in partnership with the MOEHS of Somaliland.",
+   Location: "Somaliland",
+   timeframes: "September 2018 - November 2018",
+   icon: "He-1.png",
   },
   {
    title: "CARE International",
+   classification: "Political Economy Analysis & Security Advisory",
+   description:
+    "Final Evaluation of the ‘Strengthening Civil Society and Public Engagement in Somalia’ (SCOPES) project to assess the project’s performance and achievements against the overall and specific objectives and to identify factors of success or failure, following the OECD DAC Evaluation Criteria.",
+   Location: "Somalia",
+   icon: "care-social-image-400x210.jpg",
   },
   {
    title: "Solidarites International",
+   classification: "Third Party Monitoring & Evaluation",
+   description:
+    "External Evaluation of the Emergency response to disaster affected population of IDPs and host community Implemented by Solidarites International in Lower Juba.",
+   Location: "Lower Juba, Somalia",
+   timeframes: "May 2016 - July 2016",
+   classification2: "Political Economy Analysis & Security Advisory",
+   description2:
+    "Consultancy Conflict Mediation and Resolution for ensuring the release of Solidarites International’s staff that were arrested by Armed Group in Gedo, Somalia.",
+   icon: "solidarites.jpg",
   },
   {
    title: "ACTED",
+   classification: "Political Economy Analysis & Security Advisory",
+   description: "Security Advisory on Need Basis for Jubbaland, Somalia.",
+   Location: "Jubbaland, Somalia",
+   timeframes: "January 2016 - December 2017",
+   classification2: "Political Economy Analysis & Security Advisory",
+   description2:
+    "Consultancy Conflict Mediation and Resolution for the release of ACTED’s staff that were arrested by Local Authority in Jubaland, Somalia.",
+   icon: "Acted_logo_2023.png",
   },
   {
    title:
     "DFID / Mott MacDonalds - Implementation & Analysis in Action for Accountability Project (IAAP)",
+   classification: "Formative Research & Policy Analysis",
+   description:
+    "Consortium of ALTAI and Dansom implemented an Accountability Perception Survey in Somalia. DFID / Mott McDonalds – Implementation and Analysis in Action for Accountability Programme (IAAAP) in Somalia.",
+   Location: "Somalia",
+   timeframes: " December 2015 -January 2017 ",
+   classification2: "Formative Research & Policy Analysis",
+   description2:
+    "Consortium of INTEGRITY, AXIOM and Dansom implemented an Accountability Research on accountability mechanisms along the supply chains of sesame and sugar in the context of Kismayo, Somalia. DFID / Mott Mc Donalds – Implementation and Analysis in Action of Accountability Programme (IAAAP) in Somalia.",
+   icon: "DFID-400x162.png",
   },
  ])
  let displayedProjects = ref(projects.value.slice(0, 6))
@@ -146,6 +267,19 @@
   )
   displayedProjects.value = [...displayedProjects.value, ...nextProjects]
  }
+ let searchTerm = ref("")
+ let classification = ref("")
+
+ let filteredProjects = computed(() => {
+  return displayedProjects.value.filter((project) => {
+   let matchesSearchTerm =
+    !searchTerm.value ||
+    project.title.toLowerCase().includes(searchTerm.value.toLowerCase())
+   let matchesClassification =
+    !classification.value || project.classification === classification.value
+   return matchesSearchTerm && matchesClassification
+  })
+ })
 
  let date = new Date()
  let year = date.getFullYear()
