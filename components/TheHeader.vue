68dd5864b2279f9d829eb09d404a19576aebf18a
jazimjibril <jazimjibril@gmail.com>
2024-05-20 08:13:11 +0000

    made changes

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,7 +4,7 @@
 
   <div
    class="flex flex-col items-center justify-center bg md:h-32"
-   :style="getStyleBasedOnTitle()"
+  
   >
    <h1 class="text-6xl font-bold text-center text-gray-800 font-Lora">
     {{ title }}
@@ -22,15 +22,15 @@
   projTitle: {},
  })
 
- const getStyleBasedOnTitle = computed(() => {
-  if (title === "Who we are") {
-   return {
-    background:
-     "radial-gradient(farthest-side at -33.33% 50%, #0000 52%, #474bff 54% 57%, #0000 59%) 0 calc(40px / 2), radial-gradient(farthest-side at 50% 133.33%, #0000 52%, #474bff 54% 57%, #0000 59%) calc(40px / 2) 0, radial-gradient(farthest-side at 133.33% 50%, #0000 52%, #474bff 54% 57%, #0000 59%), radial-gradient(farthest-side at 50% -33.33%, #0000 52%, #474bff 54% 57%, #0000 59%), #47d3ff",
-    backgroundSize: "calc(40px / 4.667) 40px, 40px calc(40px / 4.667)",
-   }
-  }
- })
+//  const getStyleBasedOnTitle = computed(() => {
+//   if (title === "Who we are") {
+//    return {
+//     background:
+//      "radial-gradient(farthest-side at -33.33% 50%, #0000 52%, #474bff 54% 57%, #0000 59%) 0 calc(40px / 2), radial-gradient(farthest-side at 50% 133.33%, #0000 52%, #474bff 54% 57%, #0000 59%) calc(40px / 2) 0, radial-gradient(farthest-side at 133.33% 50%, #0000 52%, #474bff 54% 57%, #0000 59%), radial-gradient(farthest-side at 50% -33.33%, #0000 52%, #474bff 54% 57%, #0000 59%), #47d3ff",
+//     backgroundSize: "calc(40px / 4.667) 40px, 40px calc(40px / 4.667)",
+//    }
+//   }
+//  })
 </script>
 
 <style scoped>
